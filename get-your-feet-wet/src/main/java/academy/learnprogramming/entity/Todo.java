a80eaffa6b8b2daa69ed263a0ec03357fd83172b
Enelith <jonathan.vinh@gmail.com>
2021-06-04 10:58:06 +0200

    Maj Part 24 - Creating the Todo Object

--- get-your-feet-wet/src/main/java/academy/learnprogramming/entity/Todo.java
+++ get-your-feet-wet/src/main/java/academy/learnprogramming/entity/Todo.java
@@ -1,5 +1,48 @@
 package academy.learnprogramming.entity;
 
-public class Todo {
+import java.time.LocalDate;
 
+public class Todo {
+    private String task;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+    private LocalDate completedDate;
+    private LocalDate createDate;
+    
+    
+    
+    
+    
+    
+    
+    public String getTask() {
+        return task;
+    }
+    public void setTask(String task) {
+        this.task = task;
+    }
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+    public LocalDate getCompletedDate() {
+        return completedDate;
+    }
+    public void setCompletedDate(LocalDate completedDate) {
+        this.completedDate = completedDate;
+    }
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
 }
