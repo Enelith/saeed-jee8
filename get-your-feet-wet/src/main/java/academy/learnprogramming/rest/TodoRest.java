53531db1e4fe5553ec8feb66b32b0da72916d0f1
Enelith <jonathan.vinh@gmail.com>
2021-06-07 14:58:22 +0200

    Maj Part 35 - Marking a Todo Object as Complete

--- get-your-feet-wet/src/main/java/academy/learnprogramming/rest/TodoRest.java
+++ get-your-feet-wet/src/main/java/academy/learnprogramming/rest/TodoRest.java
@@ -13,6 +13,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -58,5 +59,16 @@ public class TodoRest {
     @GET
     public List<Todo> getTodos() {
 	return todoService.getTodos();
+    }
+    
+    @Path("status")
+    @POST
+    public Response markAsComplete(@QueryParam("id") Long id) {
+	Todo todo = todoService.findTodoById(id);
+	todo.setCompleted(true);
+	
+	todoService.updateTodo(todo);
+	
+	return Response.ok(todo).build();
     }
 }
