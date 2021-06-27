cead1e7e21b587040128893042ce8b1dee0ba590
Enelith <jonathan.vinh@gmail.com>
2021-06-27 17:31:54 +0200

    Maj Part 372 - Stateless Security with JWT - ContainerRequestFilter Implementation Part 1

--- capstone/src/main/java/academy/learnprogramming/security/SecurityFilter.java
+++ capstone/src/main/java/academy/learnprogramming/security/SecurityFilter.java
@@ -3,9 +3,12 @@ package academy.learnprogramming.security;
 import java.io.IOException;
 
 import javax.annotation.Priority;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 import academy.learnprogramming.annotations.Authz;
@@ -21,8 +24,16 @@ public class SecurityFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-	// TODO Auto-generated method stub
-	
-    }
+	// - Grab Token from the Header of the Request using the AUTHORIZATION constant
+	// - Throw an Exception with a message if there's no Token
+	// - Parse the Token
+	// - If parsing succeed, proceed
+	// - Otherwise, throw an Exception with message
 
+	String authString = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+	if(authString == null || authString.isEmpty() || !authString.startsWith("Bearer")) {
+	    // Beautify this Exception
+	    throw new NotAuthorizedException(Response.status(Response.Status.UNAUTHORIZED).build());
+	}
+    }
 }
